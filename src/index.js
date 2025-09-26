7137415d7f8b4c53b4715165e2521b3aea0abae6
newdavemo <newdavemo@gmail.com>
2025-09-26 15:14:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
+import './styles/animations.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

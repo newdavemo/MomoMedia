2ab78f031705890f4114e8c1d5fc9b44ed47a6e4
newdavemo <newdavemo@gmail.com>
2025-09-26 15:14:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import './styles/globals.css';
-import './styles/animations.css';
+import Services from './components/Services';
+import Pricing from './components/Pricing';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Services />
+      <Pricing />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
   );
 }
